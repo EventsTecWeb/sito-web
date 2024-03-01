f844269e058d7b37938916135f71b90cf977db3e
dadiottto <davide.diotto@studenti.unipd.it>
2024-03-01 11:43:25 +0100

    file php

---
+++ database_con.php
@@ -0,0 +1,23 @@
+<?php
+
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "events_db";
+    try {
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        
+        // Verifico la connessione
+        if ($conn->connect_error) {
+            throw new Exception("Errore di connessione al database: " . $conn->connect_error);
+        }
+        // Imposto la codifica dei caratteri
+        mysqli_set_charset($conn, 'utf8mb4');
+    } catch (Exception $e) {
+
+        // Reindirizzo l'utente alla pagina di errore 500
+        header('Location: ../html/500.html');
+        exit(); // Termino lo script PHP dopo il reindirizzamento
+
+    }
+?>
