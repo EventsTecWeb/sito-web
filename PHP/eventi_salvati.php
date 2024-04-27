1caeb86dcf6637eb5a8c4fb9137176dc5fdfb9c7
Nicola Preto <nicola.preto@studenti.unipd.it>
2024-04-27 14:15:58 +0200

    aggiornato eventi_salvati.php per adattarlo alle modifiche fatte in query.php

--- PHP/eventi_salvati.php
+++ PHP/eventi_salvati.php
@@ -11,12 +11,12 @@ $eventiSalvati="";
 if (isset($_SESSION['user_id'])) {
     $template = file_get_contents("../HTML/eventiSalvati.html");
     $ris = getEventiSalvati($conn, $_SESSION['user_id']);
-    if($ris==false){
+    if(is_array($ris) && empty($ris)){
         $eventiSalvati='<p class="nessunEventoTrovatoImpo">
                         Non ci sono eventi salvati... per ora!
                         </p>';
-    }else{
-        while($evento = $ris->fetch_assoc()){
+    } else if(is_array($ris)) {
+        foreach($ris as $evento) {
             $eventiSalvati.='<div class="cardSalvato-es" id="event4Salvato-es">
             <a class="a_evento" href="../PHP/pageEvent.php?evento='.$evento["evento_id"].'" alt="premi per accedere alla pagina dell&#39evento">
                                     <div class="card-body-salvati-es">
@@ -30,13 +30,13 @@ if (isset($_SESSION['user_id'])) {
                                 </a>
                             </div>';
         }
+    } else {
+        // Gestire il caso in cui $ris non è un array (ad esempio, potrebbe essere false)
     }
 } else {
     header("Location: ../php/accessNav.php");
     exit();
 }
-
-
 
 $template = str_replace('{EVENTI}', $eventiSalvati, $template);
 echo $template;
