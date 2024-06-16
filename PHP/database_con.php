0b25d0cf5c4e21fa0747b63bc9fdd1c0b4a54263
Nicola Preto <nicola.preto@studenti.unipd.it>
2024-06-16 17:10:42 +0200

    NAVBAR FORSE FUNZIONANTEEEEEE

--- PHP/database_con.php
+++ PHP/database_con.php
@@ -2,7 +2,7 @@
     $servername = "localhost";
     $username = "root";
     $password = "";
-    $dbname = "events";
+    $dbname = "prova_tecweb";
     try {
         $conn = new mysqli($servername, $username, $password, $dbname);
                 if ($conn->connect_error) {
